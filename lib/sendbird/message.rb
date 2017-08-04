4ae4468445a492e542e92d7230880a9fa3187315
fresh2nd <jonatan.rojas@gmail.com>
2017-08-04 02:13:33 +0200

    Create edit on message

--- lib/sendbird/message.rb
+++ lib/sendbird/message.rb
@@ -5,12 +5,20 @@ module SendBird
     def self.base_url(params = {})
       "#{SendBird.configuration.site}/#{params[:channel_type]}/#{params[:channel_url]}/messages"
     end
-
+    
+    def self.edit_url(params = {})
+      "#{SendBird.configuration.site}/#{params[:channel_type]}/#{params[:channel_url]}/messages/#{params[:message_id]}"
+    end
+    
     def mark_as_read
     end
     
     def self.create(params = {}, headers = {})
       response = JSON.parse(RestClient.post(base_url(params), params.to_json, { content_type: :json, 'Api-Token': SendBird.configuration.api_key }).body)
+    end
+    
+    def self.editj(params = {}, headers = {})
+      response = SON.parse(RestClient.put(edit_url(params), params.to_json, { content_type: :json, 'Api-Token': SendBird.configuration.api_key }).body)
     end
   end
 end
