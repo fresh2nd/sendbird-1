a4e2d5d31998090be8eddc2df51203f8b3c33e8c
fresh2nd <jonatan.rojas@gmail.com>
2017-08-04 03:04:14 +0200

    Update message.rb

--- lib/sendbird/message.rb
+++ lib/sendbird/message.rb
@@ -6,7 +6,7 @@ module SendBird
       "#{SendBird.configuration.site}/#{params[:channel_type]}/#{params[:channel_url]}/messages"
     end
     
-    def self.edit_url(params = {})
+    def self.resource_url(params = {})
       "#{SendBird.configuration.site}/#{params[:channel_type]}/#{params[:channel_url]}/messages/#{params[:message_id]}"
     end
     
@@ -18,7 +18,11 @@ module SendBird
     end
     
     def self.edit(params = {}, headers = {})
-      response = JSON.parse(RestClient.put(edit_url(params), params.to_json, { content_type: :json, 'Api-Token': SendBird.configuration.api_key }).body)
+      response = JSON.parse(RestClient.put(resource_url(params), params.to_json, { content_type: :json, 'Api-Token': SendBird.configuration.api_key }).body)
+    end
+    
+    def self.view(params = {}, headers = {})
+      response = JSON.parse(RestClient.put(resource_url(params), params.to_json, { content_type: :json, 'Api-Token': SendBird.configuration.api_key }).body)
     end
   end
 end
