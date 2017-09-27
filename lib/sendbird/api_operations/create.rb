d7125e1f278850e3efedc3c59855d3defdb6dbe4
fresh2nd <jonatan.rojas@gmail.com>
2017-09-28 01:12:44 +0200

    Update create.rb

--- lib/sendbird/api_operations/create.rb
+++ lib/sendbird/api_operations/create.rb
@@ -2,7 +2,14 @@ module SendBird
   module APIOperations
     module Create
       def create(params = {}, headers = {})
-        response = JSON.parse(RestClient.post(base_url(params), params.to_json, { content_type: :json, 'Api-Token': SendBird.configuration.api_key }).body)
+        response = JSON.parse(
+          begin
+             RestClient.post(base_url(params), params.to_json, { content_type: :json, 'Api-Token': SendBird.configuration.api_key }).body
+          rescue RestClient::ExceptionWithResponse => e
+             e.response
+              puts e.response
+           end
+          )
         convert_to_sendbird_object(response, params)
       end
     end
